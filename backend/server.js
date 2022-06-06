021b9da7bb27d1bf8657b026f29996114b127f9a
Dhruv Maheshwari <mrdm026@gmail.com>
2022-06-07 01:05:10 +0530

    made changes to the port

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ app.use(passport.initialize());
 
 require("./passport")(passport);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || "3000";
 
 app.listen(PORT, console.log(`Server running on PORT: ${PORT}`));
