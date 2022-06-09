6bc133e372cfd6d0b93d75acac03fc9727470aed
Dhruv Maheshwari <mrdm026@gmail.com>
2022-06-09 17:27:15 +0530

    inital commit

---
+++ backend/src/config/dbConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: "flutter-backend-app",
+  database:
+    "mongodb+srv://dhruv-flutter:dhruv026@flutter-backend.sgo1i.mongodb.net/flutter-backend-db?retryWrites=true&w=majority",
+};
